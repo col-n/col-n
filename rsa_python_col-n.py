18a36a60b3040ad3fb81dc859f25ff1488d1f813
Colin <col-n@users.noreply.github.com>
2016-06-14 17:48:23 -0400

    update

--- rsa_python_col-n.py
+++ rsa_python_col-n.py
@@ -411,6 +411,6 @@ factor while having basically no effect on the time to create the factor.
 '''
 #for a great webcomic on passwords ...
 #a good password: https://xkcd.com/936/
-#https://drive.google.com/file/d/0B5eMNCdPTsppZ0o4YWNxQnE0MU0/view?usp=sharing
-#https://drive.google.com/file/d/0B6WU82j60QPoM3E1ZlVrZlF2QUU/view?usp=sharing
+
+#https://drive.google.com/open?id=0B5eMNCdPTsppa2ZvRVBXblZhVWM
 
